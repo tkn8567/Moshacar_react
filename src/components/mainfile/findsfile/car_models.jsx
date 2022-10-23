1db632097e8cca3a4245086906caaed60302eaa6
tkn8567 <takushinaka@gmail.com>
2022-10-24 06:25:14 +0900

    remain header and carousel

--- src/components/mainfile/findsfile/car_models.jsx
+++ src/components/mainfile/findsfile/car_models.jsx
@@ -13,28 +13,35 @@ import image_car_model12 from './car_models_img/img69.jpg';
 
 const Brands = () =>{
     const contents = [
-        {image: image_car_model1, name: 'M3', price: '190.6万円〜1458.1万円'},
-        {image: image_car_model2, name: 'シボレーカマロ', price: '62.6万円〜566.6万円'},
-        {image: image_car_model3, name: 'ランドクルーザープラド', price: '39.2万円〜628.4万円'},
-        {image: image_car_model4, name: 'LX', price: '730.6万円〜1321万円'},
-        {image: image_car_model5, name: 'RX', price: '71.6万円〜798.6万円'},
-        {image: image_car_model6, name: '86', price: '61.9万円〜510.9万円'},
-        {image: image_car_model7, name: 'ジープ・ラングラーアンリミテッド', price: '103.2万円〜813万円'},
-        {image: image_car_model8, name: 'Gクラス', price: '229.8万円〜2087.9万円'},
-        {image: image_car_model9, name: 'プリウス', price: '0万円〜304万円'},
-        {image: image_car_model10, name: 'GLE', price: '410.6万円〜1126.3万円'},
-        {image: image_car_model11, name: '911', price: '126.2万円〜2307.8万円'},
-        {image: image_car_model12, name: 'シルビア', price: '47万円〜431.7万円'}
+        {image: image_car_model1, name: 'M3', price: '190.6万円〜1458.1万円', color1: '#D9AF07', color2: '#FEFBE6'},
+        {image: image_car_model2, name: 'シボレーカマロ', price: '62.6万円〜566.6万円', color1: '#8E8E8E', color2: '#F8F8F8'},
+        {image: image_car_model3, name: 'ランドクルーザープラド', price: '39.2万円〜628.4万円', color1: '#D08163', color2: '#FCEFEC'},
+        {image: image_car_model4, name: 'LX', price: '730.6万円〜1321万円', color1: '#C1C1C1', color2: '#FFFFFF'},
+        {image: image_car_model5, name: 'RX', price: '71.6万円〜798.6万円', color1: '#C1C1C1', color2: '#FFFFFF'},
+        {image: image_car_model6, name: '86', price: '61.9万円〜510.9万円', color1: '#C1C1C1', color2: '#FFFFFF'},
+        {image: image_car_model7, name: 'ジープ・ラングラーアンリミテッド', price: '103.2万円〜813万円', color1: '#C1C1C1', color2: '#FFFFFF'},
+        {image: image_car_model8, name: 'Gクラス', price: '229.8万円〜2087.9万円', color1: '#C1C1C1', color2: '#FFFFFF'},
+        {image: image_car_model9, name: 'プリウス', price: '0万円〜304万円', color1: '#C1C1C1', color2: '#FFFFFF'},
+        {image: image_car_model10, name: 'GLE', price: '410.6万円〜1126.3万円', color1: '#C1C1C1', color2: '#FFFFFF'},
+        {image: image_car_model11, name: '911', price: '126.2万円〜2307.8万円', color1: '#C1C1C1', color2: '#FFFFFF'},
+        {image: image_car_model12, name: 'シルビア', price: '47万円〜431.7万円', color1: '#C1C1C1', color2: '#FFFFFF'}
     ];
     return (
         <>
         <div className='main_title_option'><font size='5'>人気車種から探す</font></div>
         <div className='main_car_models'>
             {contents.map((content, index) => {
+                let border = '1px solid ' + content.color1;
+                let bg_color = content.color1;
+                let card_color = content.color2;
                 return(
-                <div className='main_car_model' key={index}>
+                <div className='main_car_model' key={index} style={{
+                    background: card_color
+                }}>
                     <div className='car_model_content_top'>
-                        <div className='car_model_num'>{index+1}</div>
+                        <div className='car_model_num' style={{
+                            border: border, background: bg_color
+                        }}>{index+1}</div>
                         <div className='car_model_name'>{content.name}</div>
                     </div>
                     <div className='car_model_content_bottom'>
