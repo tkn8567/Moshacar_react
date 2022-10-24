e1c35993830100d365975a8214a96d67713aecfa
tkn8567 <takushinaka@gmail.com>
2022-10-24 09:23:05 +0900

    footer correction

--- src/components/footerfile/footer.jsx
+++ src/components/footerfile/footer.jsx
@@ -4,39 +4,39 @@ import Logo_facebook from './img88.png';
 import Logo_twitter from './img89.png';
 
 function footer() {
+  const contents = [
+    'トップ', '実績紹介', '会社概要', '査定申込',
+    'ご利用の流れ', '車種別中古車買取情報', '個人情報保護方針', 'お問い合わせ',
+    'Q&A', '中古車買取お役たちコラム', '中古車買取お役たちコラム', 'マイページログイン',
+    '評価について', 'キャンピングカーの種類と買取について', '書類・規約ダウンロード'
+  ];
   return (
     <>
       ↓footer part
       <div className='footer_box'>
-        <div className='footer_infos'>
-          <div className='footer_info'>
-            <p className='footer_info_p'><font size='1'>トップ<br/>ご利用の流れ<br/>Q&amp;A<br/>評価について</font></p>
-          </div>
-          <div className='footer_info'>
-            <p className='footer_info_p'><font size='1'>実績紹介<br/>車種別中古車買取情報<br/>中古車買取お役たちコラム<br/>キャンピングカーの種類と買取について</font></p>
-          </div>
-          <div className='footer_info'>
-            <p className='footer_info_p'><font size='1'>会社概要<br/>個人情報保護方針<br/>特定商取引法に基づく表記<br/>書類・規約ダウンロード</font></p>
-          </div>
-          <div className='footer_info'>
-            <p className='footer_info_p'><font size='1'>査定申込<br/>お問い合わせ<br/>マイページログイン</font></p>
-          </div>
+        <div className='footer_top'>
+          {contents.map((content, index) => {
+            return(
+              <div className='footer_info' key={index}>{content}</div>
+            );
+          })}
         </div>
-        <div className='footer_about_logos'>
+        <div className='footer_bottom'>
           <div className='footer_row'>
-            <img className='footer_about_logo_facebook' src={Logo_facebook} alt='logo_facebook' height='20' width='20'/><br/>
-            <img className='footer_about_logo_twitter' src={Logo_twitter} alt='logo_twitter' height='20' width='20'/><br/>
+            <img src={Logo_facebook} alt='logo_facebook' height='17' width='17'/>
+            <img src={Logo_twitter} alt='logo_twitter' height='17' width='17'/>
           </div>
+          
+          <p className='footer_about'>
+            <img src={Logo_sellca} alt='logo_sellca' height='25' width='100'/><br/>
+            <font size='1'>
+              クイック・ネットワーク株式会社<br/>
+              〒650-0041 兵庫県神戸市中央区新港町11-1 ジーライオンアワーズビル9F<br/>
+              050-3145-6925<br/>
+              &copy;SellCa ALL RIGHTS RESERVED
+            </font>
+          </p>
         </div>
-        <img src={Logo_sellca} alt='logo_sellca' height='22.8' width='90'/><br/>
-        <p className='footer_about_p'>
-          <font size='1'>
-            クイック・ネットワーク株式会社<br/>
-            〒650-0041 兵庫県神戸市中央区新港町11-1 ジーライオンアワーズビル9F<br/>
-            050-3145-6925<br/>
-            &copy;SellCa ALL RIGHTS RESERVED
-          </font>
-        </p>
       </div>
     </>
   );
