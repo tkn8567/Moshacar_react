8eec890bbcd0f224623eba085027b3856b4d75b6
tkn8567 <takushinaka@gmail.com>
2022-10-28 22:12:37 +0900

    font problem remain

--- src/components/mainfile/sellsfile/carousel.jsx
+++ src/components/mainfile/sellsfile/carousel.jsx
@@ -1,3 +1,11 @@
+import SwiperCore, { Autoplay } from 'swiper';
+import { Navigation, Pagination } from 'swiper'; // モジュールをインポート
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation'; // スタイルをインポート
+import 'swiper/css/pagination'; // スタイルをインポート
+
 import image_sell1 from './main_sell07.jpg';
 import image_sell2 from './main_sell08.jpg';
 import image_sell3 from './main_sell09.jpg';
@@ -8,8 +16,9 @@ import image_sell7 from './main_sell04.jpg';
 import image_sell8 from './main_sell05.jpg';
 import image_sell9 from './main_sell06.jpg';
 import bg_sell from './main_sell02.png';
-import Carousel from './carousel'
-const Sells = () =>{
+
+SwiperCore.use([Autoplay])
+const Slider = () => {
     const contents = [
         {image: image_sell1, name1: 'シボレーシボレーコルベット', name2: 'Z512LT', text1: 'スタート額：398.0万円', text2: '年式：2017', text3: '走行距離：10,924km', text4: '成約日：2021年07月', sold: '807.7万円', price_up: '409.7'},
         {image: image_sell2, name1: 'ベントレーミュルザンヌ', name2: 'ベースグレード', text1: '希望価格：0.0万円', text2: '年式：2013', text3: '走行距離：7,306km', text4: '成約日：2021年11月', sold: '1,074.3万円', price_up: '1,074.3'},
@@ -23,11 +32,48 @@ const Sells = () =>{
     ];
     return (
         <>
-        <div className='main_title'><font size='5'>高額売却実績</font></div>
-        <Carousel />
-        <div className='main_button'>高額売却実績をもっと見る</div>
+        <Swiper className='main_sells_carousel'
+            modules={[Navigation, Pagination]}
+            pagination={{
+                el: "#pagination",
+                clickable: true
+            }}
+            spaceBetween={25}
+            slidesPerView={5}
+            loop={true}
+            autoplay={{
+                delay: 3000,
+                disableOnInteraction: false
+            }}
+        >
+            {contents.map((content, index) => {
+                return(
+                    <SwiperSlide className='main_sell' key={index}>
+                        <div className='sell_box'>
+                        <div className='sell_name'>{content.name1}<br/>{content.name2}</div>
+                        <img src={content.image} width='144' height='95' alt='car_image' />
+                        <div className='sell_bottom'>
+                            <div className='sell_left'>
+                                {content.text1}<br/>{content.text2}<br/>{content.text3}<br/>{content.text4}
+                            </div>
+                            <div className='sell_right'>
+                                <div className='sell_sold'>
+                                    Sellca実績<br/>{content.sold}
+                                    <img src={bg_sell} width='60'/>
+                                </div>
+                                <div className='sell_price_up'>
+                                    {content.price_up}
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </SwiperSlide>
+                );
+            })}
+        </Swiper>
+        <div id="pagination" className="swiper-pagination"></div>
         </>
     );
-}
-export default Sells;
+};
 
+export default Slider;
